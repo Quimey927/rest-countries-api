48f93f76607cc9972ed6ec7ba944b67fd5101949
Quimey <quimeymata@gmail.com>
2022-12-02 18:03:05 -0300

    Adds sun icon to the header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 import ThemeContext from '../../store/theme-context';
 import classes from './Header.module.css';
@@ -12,12 +12,16 @@ const Header = () => {
     themeCtx.isDarkThemeActive ? 'dark-element shadow' : 'shadow'
   }`;
 
+  const icon = themeCtx.isDarkThemeActive ?
+  <FontAwesomeIcon className={classes.icon} icon={faSun} /> :
+  <FontAwesomeIcon className={classes.icon} icon={faMoon} />
+
   return (
     <header className={headerClasses}>
       <h3>Where in the world?</h3>
       <button onClick={themeCtx.onToggleTheme}>
-        <FontAwesomeIcon className={classes.icon} icon={faMoon} />
-        Dark mode
+        {icon}
+        {themeCtx.isDarkThemeActive ? 'Light mode' : 'Dark mode'}
       </button>
     </header>
   );
